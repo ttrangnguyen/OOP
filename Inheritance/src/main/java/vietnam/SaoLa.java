077455ce75e12b1a3251f1b3720f5da4e7dc0082
ttrangnguyen <nguyenthutrang.uet@gmail.com>
2024-10-09 21:33:09 +0700

    Revert "Revert "Example code for inheritance lecture""
    This reverts commit d1f42c7650d5f642da44c2a383faf43dac4ddc80.

---
+++ Inheritance/src/main/java/vietnam/SaoLa.java
@@ -0,0 +1,11 @@
+package vietnam;
+
+import org.example.Animal;
+
+public class SaoLa extends Animal {
+    public void eat(){
+        System.out.println("SaoLa " + name +
+                "is eating " + food);
+    }
+
+}
